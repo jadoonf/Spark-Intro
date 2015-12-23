6d521c4b84a1deddfeddb898042ef61300526bb9
fkjadoon94 <fkjadoon94@gmail.com>
2015-12-23 17:52:18 -0500

    organized code and added comments

--- basic-programs-ec2-cluster/src/main/scala/WhiteHouse.scala
+++ basic-programs-ec2-cluster/src/main/scala/WhiteHouse.scala
@@ -1,60 +1,58 @@
 /* WhiteHouse.scala */
-/* For cluster */
+/* For running on cluster */
 
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.sql.SQLContext._
+
 //import com.databricks.spark.csv
-//import com.databricks.spark.csv._
+//import com.databricks.spark.csv._   //These libraries are currently being imported in the spark-submit script (see docs) 
 
 object WhiteHouse {
 
-	val MASTER = "ec2-52-23-200-195.compute-1.amazonaws.com" // changing to ip / public dns might work
+	val MASTER = "[master_public_dns]" // Can be changed to the ip address too
   	val SPARK_MASTER = "spark://" + MASTER + ":7077"
   	val HDFS = "hdfs://" + MASTER + ":9000"
 
-	val visitorsDir = HDFS +"/Whitehouse-output/visitors" //change path for cluster and local accordingly 
+  	//change the following paths for your own cluster accordingly
+
+	val visitorsDir = HDFS +"/Whitehouse-output/visitors" 
 	val visiteesDir = HDFS +"/Whitehouse-output/visitees"
 	val combinationDir = HDFS +"/Whitehouse-output/combination"
 
 	def main(args: Array[String]) {
 		
 		val conf = new SparkConf()
-		.setMaster("spark://ec2-52-23-200-195.compute-1.amazonaws.com:7077")   
-      	.setAppName("hw5")  
+		.setMaster(MASTER)   
+      	.setAppName("basic")  
 
 		val sc = new SparkContext(conf)
 
 		val sqlContext = new SQLContext(sc) 
-		
-		
 
 		val df = sqlContext
 		.read
 		.format("com.databricks.spark.csv") 
 		.option("header", "true")
-		.load("s3n://access_id:secret_key@spark-farrukh/whitehouse_waves-2015_09.csv") //change path for cluster and local accordingly 
+		.load("s3n://[access_id:secret_key]@spark-farrukh/whitehouse_waves-2015_09.csv") //change to your respective s3 bucket path 
 		
-
-//Create table for SQL querying
-
+		//Specifying the schema by inference
 
 		val dfTable = df.toDF()
 
 		dfTable.registerTempTable("whitehouse_table")
 
+//application logic
 
 //Most frequent vistors
-
-		
 
 		val vistorsQuery = sqlContext
 		.sql("SELECT NAMELAST, NAMEFIRST, NAMEMID FROM whitehouse_table")
 
-		vistorsQuery.explain(true)
+		vistorsQuery.explain(true) //prints schema information at runtime 
 
-		val projectVisitorsQuery = vistorsQuery.map( name => ( name, 1 ))      ////as (k,v) 
+		val projectVisitorsQuery = vistorsQuery.map( name => ( name, 1 )) //as (k,v) pairs 
 
 		val reduceVisitorsQuery = projectVisitorsQuery.reduceByKey(_+_)
 
@@ -62,20 +60,17 @@ object WhiteHouse {
 		
 		.takeOrdered(10)(Ordering[Int].reverse.on(x => x._2))
 
-
 		val visitorsRDD = sc.parallelize(sortVisitorsQuery)
 		.saveAsTextFile(visitorsDir)
 
 //Most frequent visitees
-		
-	
 
 		val visiteesQuery = sqlContext
 		.sql("SELECT visitee_namefirst, visitee_namelast FROM whitehouse_table")
 
 		visiteesQuery.explain(true)
 
-		val projectVisiteesQuery = visiteesQuery.map( name => ( name, 1 ))      ////as (k,v) 
+		val projectVisiteesQuery = visiteesQuery.map( name => ( name, 1 ))      //as (k,v) pairs
 
 		val reduceVisiteesQuery = projectVisiteesQuery.reduceByKey(_+_)
 
@@ -86,31 +81,22 @@ object WhiteHouse {
 		val visiteesRDD = sc.parallelize(sortVisiteesQuery)
 		.saveAsTextFile(visiteesDir)
 
-
 //Most frequent combination
-
 
 		val combinationQuery = sqlContext
 		.sql("SELECT NAMELAST, NAMEFIRST, NAMEMID, visitee_namefirst, visitee_namelast FROM whitehouse_table")
 
 		combinationQuery.explain(true)
 
-		val projectCombinationQuery = combinationQuery.map( name => ( name, 1 ))      ////as (k,v) 
+		val projectCombinationQuery = combinationQuery.map( name => ( name, 1 ))      //as (k,v) pairs 
 
 		val reduceCombinationQuery = projectCombinationQuery.reduceByKey(_+_)
-
-
 		
 		val sortCombinationQuery = reduceCombinationQuery.takeOrdered(10)(new Ordering[(org.apache.spark.sql.Row, Int)] {
       	override def compare(x: (org.apache.spark.sql.Row, Int), y: (org.apache.spark.sql.Row, Int)) = { -x._2.compareTo(y._2) }
-    	});
-
-    	
+    	});    	
 		val combinationRDD = sc.parallelize(sortCombinationQuery)
 		.saveAsTextFile(combinationDir)
-
-
-
 
 	}
 }
--- basic-programs-ec2-cluster/src/main/scala/Wikipedia.scala
+++ basic-programs-ec2-cluster/src/main/scala/Wikipedia.scala
@@ -1,5 +1,5 @@
 /* Wikipedia.scala */
-/* For cluster */
+/* For running on cluster */
 
 
 import org.apache.spark.SparkContext
@@ -12,21 +12,21 @@ import org.apache.spark.sql.SQLContext._
 object Wikipedia {
 
 
-  val MASTER = "ec2-52-23-200-195.compute-1.amazonaws.com" // changing to ip / public dns might work
+  val MASTER = "[master_public_dns]" // Can be changed to the ip address too
   val SPARK_MASTER = "spark://" + MASTER + ":7077"
   val HDFS = "hdfs://" + MASTER + ":9000"
 
-  val outputDirNoOutlinks = HDFS + "/Wikipedia-output/no-outlinks"
+  //change the following paths for your own cluster accordingly
+
+  val outputDirNoOutlinks = HDFS + "/Wikipedia-output/no-outlinks" 
   val outputDirNoInlinks = HDFS + "/Wikipedia-output/no-inlinks"
  
-  	
-  
   def main(args: Array[String]) {
     
     
     	val conf = new SparkConf()
-    	.setMaster("spark://ec2-52-23-200-195.compute-1.amazonaws.com:7077")  
-      .setAppName("hw5")  
+    	.setMaster(MASTER)  
+      .setAppName("basic")  
 	
 
 		  val sc = new SparkContext(conf)
@@ -34,56 +34,38 @@ object Wikipedia {
         
 
   		val wikiData = sc
-  		.textFile("s3n://access_id:secret_key@spark-farrukh/links-simple-sorted.txt") //change for ubuntu
+  		.textFile("s3n://[access_id:secret_key]@spark-farrukh/links-simple-sorted.txt") //change to your respective s3 bucket path 
 
-  	
+  		//specifying the dataframes schema progmatically
 
-
-  		val schemaString = "page_id outlinks_page_id" 
-
+      val schemaString = "page_id outlinks_page_id"   
 
   		import org.apache.spark.sql.Row;   
   		import org.apache.spark.sql.types.{StructType,StructField,StringType};
-
-
 
   		val schema =
   	  		StructType(
         		schemaString.split(" ").map(fieldName => StructField(fieldName, StringType, true))) 
 
-
   		val wikiDataRDD = wikiData.map(_.split(":")).map(l => Row(l(0), l(1).trim)) 
   	
-
-
 		  val wikiDataFrame = sqlContext.createDataFrame(wikiDataRDD, schema) 
 
-  	
+      //application logic
+
   		wikiDataFrame.registerTempTable("row1_and_row2_table")
-
-
-
 
     	val lhsQuery = sqlContext
     		.sql("SELECT page_id FROM row1_and_row2_table")
         .distinct 
         lhsQuery.explain(true)
-    		
-         
-
-	
-			
-
 
    	 	val rhsQuery = sqlContext
     		.sql("SELECT outlinks_page_id FROM row1_and_row2_table")
 
         rhsQuery.explain(true)
 		
-		import sqlContext.implicits._
-
-
-		
+		import sqlContext.implicits._ //for .toDF() conversion 
 
 		val rhsToString = rhsQuery
       
@@ -92,12 +74,7 @@ object Wikipedia {
   			.toDF()
   			.distinct
 
-		
-
 		val titlesData = sc.textFile("s3n://access_id:secret_key@spark-farrukh/titles-sorted.txt")
-
-
-
 
     val titlesDataFrame = titlesData.zipWithIndex.map{case(k,v) =>(v,k)}.toDF() 
 
@@ -105,20 +82,12 @@ object Wikipedia {
 
     val indices = sqlContext.sql("SELECT _1 FROM titles")
 
-    indices.explain(true)
-
-	
-	
-  
+    indices.explain(true) 
 
 		val NoOutlinksRDD = indices.except(lhsQuery).rdd   
       	.saveAsTextFile(outputDirNoOutlinks)
      
-       
     val NoInlinksRDD = indices.except(rhsToString).rdd 
       	.saveAsTextFile(outputDirNoInlinks)
-
-
-
 	}
 }
